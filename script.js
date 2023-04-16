37b4019d799622c244394f384f02a1c900780644
Diego C. Pimentel <97416706+DiegoCPJPKR@users.noreply.github.com>
2023-04-15 22:52:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let teacherForms = document.getElementsByClassName("teacher-contract")
+for (let teacherForm of teacherForms) {
+    teacherForm.addEventListener("submit", function (event) {
+        event.preventDefault()
+        console.log("Executou o submit")
+
+        let price = teacherForm.price.value
+        let hours = teacherForm.hours.value
+        let total = price * hours
+
+        alert("The total value of the contract is $" + total)
+    })
+}
